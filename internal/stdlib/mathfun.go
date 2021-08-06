2046797ec9c311383dca26a67af3a130f4679909
vulogov <vlaimir.ulogov@me.com>
2021-08-06 15:46:34 -0600

    chore: More math functions

--- internal/stdlib/mathfun.go
+++ internal/stdlib/mathfun.go
@@ -23,7 +23,7 @@ func MathSinElement(vm *vmmod.VM, e *vmmod.Elem) (*vmmod.Elem, error) {
 }
 
 func MathCosElement(vm *vmmod.VM, e *vmmod.Elem) (*vmmod.Elem, error) {
-	vm.Debug("SIN(): %v", e.Type)
+	vm.Debug("COS(): %v", e.Type)
 	switch e.Type {
 	case "flt":
 		return &vmmod.Elem{Type: "flt", Value: math.Cos(e.Value.(float64))}, nil
@@ -38,12 +38,12 @@ func MathCosElement(vm *vmmod.VM, e *vmmod.Elem) (*vmmod.Elem, error) {
 }
 
 func MathTanElement(vm *vmmod.VM, e *vmmod.Elem) (*vmmod.Elem, error) {
-	vm.Debug("SIN(): %v", e.Type)
+	vm.Debug("TAN(): %v", e.Type)
 	switch e.Type {
 	case "flt":
-		return &vmmod.Elem{Type: "flt", Value: math.Sin(e.Value.(float64))}, nil
+		return &vmmod.Elem{Type: "flt", Value: math.Tan(e.Value.(float64))}, nil
 	case "int":
-		return &vmmod.Elem{Type: "flt", Value: math.Sin(float64(e.Value.(*big.Int).Int64()))}, nil
+		return &vmmod.Elem{Type: "flt", Value: math.Tan(float64(e.Value.(*big.Int).Int64()))}, nil
 	case "dblock":
 		return DblockWalk(vm, e, math.Tan)
 	case "MAT":
@@ -53,16 +53,61 @@ func MathTanElement(vm *vmmod.VM, e *vmmod.Elem) (*vmmod.Elem, error) {
 }
 
 func MathSqrtElement(vm *vmmod.VM, e *vmmod.Elem) (*vmmod.Elem, error) {
-	vm.Debug("SIN(): %v", e.Type)
+	vm.Debug("SQRT(): %v", e.Type)
 	switch e.Type {
 	case "flt":
-		return &vmmod.Elem{Type: "flt", Value: math.Sin(e.Value.(float64))}, nil
+		return &vmmod.Elem{Type: "flt", Value: math.Sqrt(e.Value.(float64))}, nil
 	case "int":
-		return &vmmod.Elem{Type: "flt", Value: math.Sin(float64(e.Value.(*big.Int).Int64()))}, nil
+		return &vmmod.Elem{Type: "flt", Value: math.Sqrt(float64(e.Value.(*big.Int).Int64()))}, nil
 	case "dblock":
 		return DblockWalk(vm, e, math.Sqrt)
 	case "MAT":
 		return MatrixWalk(vm, e, math.Sqrt)
+	}
+	return e, nil
+}
+
+func MathExpElement(vm *vmmod.VM, e *vmmod.Elem) (*vmmod.Elem, error) {
+	vm.Debug("EXP(): %v", e.Type)
+	switch e.Type {
+	case "flt":
+		return &vmmod.Elem{Type: "flt", Value: math.Exp(e.Value.(float64))}, nil
+	case "int":
+		return &vmmod.Elem{Type: "flt", Value: math.Exp(float64(e.Value.(*big.Int).Int64()))}, nil
+	case "dblock":
+		return DblockWalk(vm, e, math.Exp)
+	case "MAT":
+		return MatrixWalk(vm, e, math.Exp)
+	}
+	return e, nil
+}
+
+func MathLogElement(vm *vmmod.VM, e *vmmod.Elem) (*vmmod.Elem, error) {
+	vm.Debug("LOG(): %v", e.Type)
+	switch e.Type {
+	case "flt":
+		return &vmmod.Elem{Type: "flt", Value: math.Log(e.Value.(float64))}, nil
+	case "int":
+		return &vmmod.Elem{Type: "flt", Value: math.Log(float64(e.Value.(*big.Int).Int64()))}, nil
+	case "dblock":
+		return DblockWalk(vm, e, math.Log)
+	case "MAT":
+		return MatrixWalk(vm, e, math.Log)
+	}
+	return e, nil
+}
+
+func MathLog10Element(vm *vmmod.VM, e *vmmod.Elem) (*vmmod.Elem, error) {
+	vm.Debug("LOG10(): %v", e.Type)
+	switch e.Type {
+	case "flt":
+		return &vmmod.Elem{Type: "flt", Value: math.Log10(e.Value.(float64))}, nil
+	case "int":
+		return &vmmod.Elem{Type: "flt", Value: math.Log10(float64(e.Value.(*big.Int).Int64()))}, nil
+	case "dblock":
+		return DblockWalk(vm, e, math.Log10)
+	case "MAT":
+		return MatrixWalk(vm, e, math.Log10)
 	}
 	return e, nil
 }
@@ -73,5 +118,7 @@ func InitMathFunctions(vm *vmmod.VM) {
 	vm.AddFunction("math/Cos", MathCosElement)
 	vm.AddFunction("math/Tan", MathTanElement)
 	vm.AddFunction("math/Sqrt", MathSqrtElement)
-
+	vm.AddFunction("math/Exp", MathExpElement)
+	vm.AddFunction("math/Log", MathLogElement)
+	vm.AddFunction("math/Log10", MathLog10Element)
 }
