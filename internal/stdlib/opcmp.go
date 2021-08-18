f0683b619d698220734cd7f211b62b521f282893
vulogov <vlaimir.ulogov@me.com>
2021-08-17 23:13:38 -0600

    chore: AND OR NOT logical ops, <= and >= cmp operators

--- internal/stdlib/opcmp.go
+++ internal/stdlib/opcmp.go
@@ -28,6 +28,28 @@ func runcmp(vm *vmmod.VM, e1 *vmmod.Elem, e2 *vmmod.Elem, cmpf func(*vmmod.VM, i
 	return &vmmod.Elem{Type: "bool", Value: cmpf(vm, res)}, nil
 }
 
+func runcmporeq(vm *vmmod.VM, e1 *vmmod.Elem, e2 *vmmod.Elem, cmpf func(*vmmod.VM, int) bool) (*vmmod.Elem, error) {
+	var cmpv *vmmod.Elem
+	cmpv = e2
+	if e1.Type != e2.Type {
+		if e1.Type == "glob" {
+			eh, err := vm.GetType(e2.Type)
+			if err != nil {
+				vm.OnError(err, "Error in logical operator for datatype: %v", e2.Type)
+			}
+			cmpv = &vmmod.Elem{Type: "str", Value: eh.ToString(vm, e2)}
+		} else {
+			return nil, fmt.Errorf("Datatypes for the glob operations are not valid: %v %v", e1.Type, e2.Type)
+		}
+	}
+	eh, err := vm.GetType(e1.Type)
+	if err != nil {
+		return nil, err
+	}
+	res := eh.Compare(vm, e1, cmpv)
+	return &vmmod.Elem{Type: "bool", Value: (cmpf(vm, res) || iseq(vm, res))}, nil
+}
+
 func iseq(vm *vmmod.VM, res int) bool {
 	if res == vmmod.Eq {
 		return true
@@ -75,8 +97,58 @@ func LSqOperator(vm *vmmod.VM, e1 *vmmod.Elem, e2 *vmmod.Elem) (*vmmod.Elem, err
 	return runcmp(vm, e1, e2, isls)
 }
 
+func LSEQqOperator(vm *vmmod.VM, e1 *vmmod.Elem, e2 *vmmod.Elem) (*vmmod.Elem, error) {
+	return runcmporeq(vm, e1, e2, isls)
+}
+
 func GTqOperator(vm *vmmod.VM, e1 *vmmod.Elem, e2 *vmmod.Elem) (*vmmod.Elem, error) {
 	return runcmp(vm, e1, e2, isgt)
+}
+
+func GTEQqOperator(vm *vmmod.VM, e1 *vmmod.Elem, e2 *vmmod.Elem) (*vmmod.Elem, error) {
+	return runcmporeq(vm, e1, e2, isgt)
+}
+
+func NOTElement(vm *vmmod.VM, e *vmmod.Elem) (*vmmod.Elem, error) {
+	if e.Type != "bool" {
+		vm.Debug("Element on the stack is not 'bool' for NOT: %v", e.Type)
+		return e, nil
+	}
+	return &vmmod.Elem{Type: "bool", Value: (!e.Value.(bool))}, nil
+}
+
+func AndFunction(vm *vmmod.VM, e *vmmod.Elem) (*vmmod.Elem, error) {
+	if e.Type != "bool" {
+		vm.Debug("Element on the stack is not 'bool' for AND: %v", e.Type)
+		return e, nil
+	}
+	if !vm.CanGet() {
+		return nil, fmt.Errorf("Stack is too shallow for AND")
+	}
+	e1 := vm.Take()
+	if e1.Type != "bool" {
+		vm.Put(e1)
+		vm.Debug("Element on the stack is not 'bool' for AND: %v", e1.Type)
+		return e, nil
+	}
+	return &vmmod.Elem{Type: "bool", Value: (e.Value.(bool) && e1.Value.(bool))}, nil
+}
+
+func OrFunction(vm *vmmod.VM, e *vmmod.Elem) (*vmmod.Elem, error) {
+	if e.Type != "bool" {
+		vm.Debug("Element on the stack is not 'bool' for OR: %v", e.Type)
+		return e, nil
+	}
+	if !vm.CanGet() {
+		return nil, fmt.Errorf("Stack is too shallow for OR")
+	}
+	e1 := vm.Take()
+	if e1.Type != "bool" {
+		vm.Put(e1)
+		vm.Debug("Element on the stack is not 'bool' for OR: %v", e1.Type)
+		return e, nil
+	}
+	return &vmmod.Elem{Type: "bool", Value: (e.Value.(bool) || e1.Value.(bool))}, nil
 }
 
 func InitOpCmp(vm *vmmod.VM) {
@@ -84,5 +156,13 @@ func InitOpCmp(vm *vmmod.VM) {
 	vm.AddOperator("=", EqOperator)
 	vm.AddOperator("<>", NEqOperator)
 	vm.AddOperator("<", GTqOperator)
+	vm.AddOperator("<=", GTEQqOperator)
 	vm.AddOperator(">", LSqOperator)
+	vm.AddOperator(">=", LSEQqOperator)
+	vm.AddFunction("NOT", NOTElement)
+	vm.AddFunction("¬", NOTElement)
+	vm.AddFunction("AND", AndFunction)
+	vm.AddFunction("∧", AndFunction)
+	vm.AddFunction("OR", OrFunction)
+	vm.AddFunction("∨", OrFunction)
 }
